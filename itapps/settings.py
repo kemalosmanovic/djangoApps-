d929fe7ee31d166ca8982f238fa45e4f96e7715e
kemalosmanovic <c0013667@hallam.shu.ac.uk>
2023-03-15 17:36:50 +0000

    Retry

--- itapps/settings.py
+++ itapps/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.enviorn.get('django-insecure-b4nbys7k0=ot04rqwzijquytal5wmim5g5v*)!yk7$pr52iw+t')
+SECRET_KEY = os.environ.get('django-insecure-b4nbys7k0=ot04rqwzijquytal5wmim5g5v*)!yk7$pr52iw+t')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
